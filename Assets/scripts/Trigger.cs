88756527f4c74f8515dca5639421052c9f382968
RTU MIREA <rtumirea@mirea.ru>
2019-11-22 11:04:09 +0300

    Initial commit

---
+++ Assets/scripts/Trigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject obj;
+    public int Score_Player = 0;
+    private float time = 0;
+    float save_time;
+
+
+
+
+
+    private void Update()
+    {
+        time++;
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Score_Player += 1;
+        }
+        if (Score_Player == 12)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            if (time <= 30)
+            {
+                Score_Player = Score_Player * 2;
+            }
+            if (time>30 && time < 60)
+            {
+                Score_Player += Score_Player / 2;
+                
+            }
+            
+        }
+        
+    }
+    
+
+}
+
+        
+
+
+
+
